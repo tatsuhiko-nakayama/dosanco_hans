d1caca6cb32411f7fa35f9c95627d29b8d3566a1
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-10-09 19:08:52 +0900

    ユーザーテーブル作成
    ・deviseインストール
    ・マイグレーション
    ・バリデーション
    ・アソシエーション

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :products
+  has_many :orders
+  has_many :comments
+  has_one :card
+
+  validates :name, :nickname, presence: true
+end
