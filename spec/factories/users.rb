61ab5bf289c21c9ca8e117bfe3617fd72828cec3
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-10-09 22:51:44 +0900

    Rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { '北海　太郎'}
+    name { '北海　太郎' }
     nickname { Faker::Name.initials(number: 2) }
     email { Faker::Internet.free_email }
     password { 'test1234' }
