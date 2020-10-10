5a44a3f0cc5af72933b5b8c636fc1f8176685355
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-10-10 14:49:14 +0900

    Cardモデル作成・JSファイル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
   has_many :comments
-  has_one :card
+  has_one :card, dependent: :destroy
 
   with_options presence: true do
     validates :name
