2d7054f9f81ff44cac276833714c1c4292716965
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-10-10 19:16:34 +0900

    Cardモデルの単体テストコード

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :card do
-    
+    card_token { 'abcdefg' }
+    customer_token { 'hijklmn' }
+    association :user
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'カード登録' do
+    before do
+      @card = FactoryBot.build(:card)
+    end
+
+    context 'カード登録がうまくいくとき' do
+      it 'card_token,customer_tokenが存在すれば登録できる' do
+        expect(@card).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it 'card_tokenが空では登録できない' do
+        @card.card_token = nil
+        @card.valid?
+        expect(@card.errors.full_messages).to include("Card token can't be blank")
+      end
+
+      it 'customer_tokenが空では登録できない' do
+        @card.customer_token = nil
+        @card.valid?
+        expect(@card.errors.full_messages).to include("Customer token can't be blank")
+      end
+    end
+  end
 end
