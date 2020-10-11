62f80ff635ee4ce72ec3abc1f73f8790b4396bf6
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-10-11 17:04:28 +0900

    購入関連の遷移ブロック

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,43 @@
 class OrdersController < ApplicationController
+  before_action :find_item, only: [:index, :create]
+  before_action :block_index, only: :index
+  before_action :block_no_card, only: :index
 
   def index
-    redirect_to user_path(current_user.id) unless current_user.card
-    
-      @item = Item.find(params[:item_id])
-
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    card = Card.find_by(user_id: current_user.id)
-    if current_user.card
-      customer = Payjp::Customer.retrieve(card.customer_token)
-      @card = customer.cards.first
-    end
-
     @order = OrderAddress.new
+    find_card
   end
 
   def create
-    @item = Item.find(params[:item_id])
+    @order = OrderAddress.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
+    else
+      find_card
+      render :index
+    end
+  end
 
+  private
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :city, :house_num, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def find_card
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    card = Card.find_by(user_id: current_user.id)
+    customer = Payjp::Customer.retrieve(card.customer_token)
+    @card = customer.cards.first
+  end
+  
+  def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     customer_token = current_user.card.customer_token
     Payjp::Charge.create(
@@ -25,19 +45,14 @@ class OrdersController < ApplicationController
       customer: customer_token,
       currency: 'jpy'
       )
-
-    @order = OrderAddress.new(order_params)
-    if @order.save
-      redirect_to root_path
-    else
-      render :index
-    end
   end
 
-  private
+  def block_index
+    redirect_to item_path(@item.id) if @item.order || @item.user == current_user
+  end
 
-  def order_params
-    params.require(:order_address).permit(:postal_code, :city, :house_num, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  def block_no_card
+    redirect_to user_path(current_user.id) unless current_user.card
   end
 
 end
--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -3,7 +3,7 @@ class OrderAddress
   attr_accessor :postal_code, :city, :house_num, :building, :phone, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
     validates :city
     validates :house_num
     validates :phone, format: { with: /\A\d{10}$|^\d{11}\z/, message: 'Input correctly' }
