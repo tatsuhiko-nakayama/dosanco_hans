32c5b4dca8030543358b1b2dc260f2c22b3063f9
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-10-11 11:48:11 +0900

    作品の編集/削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,12 +19,25 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
   
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   def show
