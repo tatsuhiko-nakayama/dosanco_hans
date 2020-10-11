f02be1e4063bae72f35e14cc78615e6d9ca1134e
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-10-11 10:17:37 +0900

    出品（DB保存)機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :price, :category_id, :from_id, :day_id).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'cards/new'
   devise_for :users
   root to: 'items#index'
   resources :users, only: [:edit, :update, :destroy, :show]
   resources :users do
     resources :cards, only: [:new, :create, :destroy]
   end
+  resources :items, only: [:index, :new, :create]
 end
