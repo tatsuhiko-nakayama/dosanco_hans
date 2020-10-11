1413caa2c838d9396ce4eb486fe71048a8647bf1
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-10-11 12:39:52 +0900

    OrderAddressクラスの作成/バリデーションの設定

---
+++ app/forms/order_address.rb
@@ -0,0 +1,16 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :city, :house_num, :building, :phone, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postalcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
+    validates :city
+    validates :house_num
+    validates :phone, format: { with: /\A\d{10}$|^\d{11}\z/, message: 'Input correctly' }
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, city: city, house_num: house_num, building: building, phone: phone, order_id: order.id)
+  end
+end
