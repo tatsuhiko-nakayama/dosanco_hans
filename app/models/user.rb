32084ca49000fe5b8c3d12f2fe562ed0daeda5c7
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-10-12 09:05:50 +0900

    モデルの編集(アソシ・OmniAuth設定)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
-  has_many :items
+  has_many :items, dependent: :destroy
   has_many :orders
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  has_many :sns_credentials, dependent: :destroy
   has_one :card, dependent: :destroy
 
   with_options presence: true do
