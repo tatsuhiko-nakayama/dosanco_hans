0478edfb58f62ddb19646b8b5c78bf94e552e4c1
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-10-13 12:12:46 +0900

    商品名検索機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,4 +17,12 @@ class Item < ApplicationRecord
   validates :price, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
   validates :price, numericality: { greater_than_or_equal_to: 100, less_than_or_equal_to: 9_999_999, message: 'が範囲外です' }
   validates :category_id, :from_id, :day_id, numericality: { other_than: 1, message: 'を選んでください' }
+
+  def self.search(search)
+    if search != ""
+      Item.where('name LIKE(?)', "%#{search}%").order('created_at DESC')
+    else
+      Item.includes(:order).order('created_at DESC')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders, only: [:index, :create]
     resources :comments, only: [:create, :destroy]
+    collection do
+      get 'search'
+    end
   end
 
   resources :categories do
